1d9201332f7d37d3537f02b958683948805fbbbd
Bryd3n <bc151106uni@gmail.com>
2026-07-12 21:25:01 -0400

    Integrate Supabase Auth

---
+++ menu-qr-web/supabase-init.js
@@ -0,0 +1,3 @@
+const supabaseUrl = 'https://rxhbqfsdtcuuiqjatmsz.supabase.co';
+const supabaseKey = 'sb_publishable_pntmwuM0rVJRLlVHHWKmeQ_DUvA_eq0';
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
